507e9134440af525bdcc3fe40296b06c1a28a05a
DPMol <12denismoldovan@gmail.com>
2022-11-29 23:01:48 +0200

    Added Backend

---
+++ Backend/src/test/java/SocialNetworkServer/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SocialNetworkServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
