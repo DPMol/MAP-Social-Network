5afe2d6bd23ac7414a9a09bbdb70b1d0457b91cd
DPMol <12denismoldovan@gmail.com>
2023-04-10 22:35:42 +0300

    a

--- Backend/src/main/java/backend/Repositories/MessageRepository.java
+++ Backend/src/main/java/backend/Repositories/MessageRepository.java
@@ -1,6 +1,5 @@
 package backend.Repositories;
 
-import backend.Domain.Friendship;
 import backend.Domain.Message;
 import backend.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,7 +13,8 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
     @Query(
             value = "SELECT f FROM Message f " +
                     "WHERE (f.sender = ?1 and f.receiver = ?2) " +
-                    "or (f.sender = ?2 and f.receiver = ?1) "
+                    "or (f.sender = ?2 and f.receiver = ?1)" +
+                    "order by f.sentDate asc"
     )
     Optional<List<Message>> findMessages(User user1, User user2);
 }
